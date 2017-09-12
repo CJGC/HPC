8a70a2873fdcfdf3eb22b6876d0346a936414e4e
cjgc <chjugomez@utp.edu.co>
2017-09-12 15:14:10 -0500

    Int data type was changed by size_t. Two unnecessary lines were deleted

--- homework1/matrix.c
+++ homework1/matrix.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 FILE * openFile(char const *fileName,FILE *f){
   /* This function will try to open a file */
@@ -8,10 +9,10 @@ FILE * openFile(char const *fileName,FILE *f){
   return f;
 }
 
-float * buildMatrix(FILE *f,int &rows,int &columns){
+float * buildMatrix(FILE *f,size_t &rows,size_t &columns){
   /* This function will build a matrix M */
-  fscanf(f,"%d",&rows);
-  fscanf(f,"%d",&columns);
+  fscanf(f,"%zu",&rows);
+  fscanf(f,"%zu",&columns);
   fgetc(f); /* skipping nasty character */
   float *M = (float *)malloc(rows*columns*sizeof(float));
   return M;
@@ -20,7 +21,7 @@ float * buildMatrix(FILE *f,int &rows,int &columns){
 void getData(float *M,FILE *f){
   /* This function will capture data from plain text file to system memory */
   char *data = (char *)malloc(sizeof(char)), *newData = NULL,ch = ' ';
-  int dataSize = sizeof(char), Mindex = 0;
+  size_t dataSize = sizeof(char), Mindex = 0;
   while(!feof(f)){
     ch = fgetc(f);
     if(ch == ',' || ch == '\n'){
@@ -41,37 +42,35 @@ void getData(float *M,FILE *f){
   free(data);
 }
 
-void hardrive(float *M,int Mr,int Mc){
+void hardrive(float *M,size_t Mr,size_t Mc){
   /*
      This function will write the result in hardrive
      M -> Matrix, Mr -> Matrix rows, Mc -> Matrix columns
   */
   FILE *f = fopen("output.txt","w+");
-  for(int i=0;i<Mr;i++){
-    for(int j=0;j<Mc;j++){
-      if(j+1 == Mc) fprintf(f,"%.1f",M[i*Mc + j]);
+  for(size_t i=0;i<Mr;i++)
+    for(size_t j=0;j<Mc;j++){
+      if(j+1 == Mc) fprintf(f,"%.1f\n",M[i*Mc + j]);
       else fprintf(f,"%.1f,",M[i*Mc + j]);
     }
-    fprintf(f,"%c",'\n');
-  }
   fclose(f);
 }
 
-void mulMatrices(float *M1,int M1r,int M1c,float *M2,int M2r,int M2c){
+void mulMatrices(float *M1,size_t M1r,size_t M1c,float *M2,size_t M2r,size_t M2c){
   /*
     This function will multiply two matrices (M1,M2)
      M1 -> Matrix1, M2 -> Matrix2, M1r -> Matrix1 rows, M1c -> Matrix1
      columns, M2r -> Matrix2 rows, M2c -> Matrix2 columns
   */
   if(M1c != M2r){printf("Matrices cannot be multiply!"); return;}
-  int M3size = M1r*M2c, M3index = 0;
+  size_t M3size = M1r*M2c;
   float M3[M3size]; /* M3 -> Matrix3 will contain the result */
 
-  for(int i=0; i<M1r; i++)
-    for(int j=0; j<M2c; j++,M3index++){
+  for(size_t i=0; i<M1r; i++)
+    for(size_t j=0; j<M2c; j++){
       float data = 0.0;
-      for(int k=0; k<M1c; k++) data = M1[i*M1c+k] * M2[k*M2c+j] + data;
-      M3[M3index] = data;
+      for(size_t k=0; k<M1c; k++) data = M1[i*M1c+k] * M2[k*M2c+j] + data;
+      M3[i*M1c+j] = data;
     }
   hardrive(M3,M1r,M2c);
 }
@@ -80,7 +79,7 @@ int main(int argc, char const *argv[]) {
   if(argc != 3){printf("There should be 3 arguments!\n");exit(1);}
   FILE *f1=NULL, *f2=NULL; /* file pointers */
   float *M1, *M2; /* matrices (M1,M2) */
-  int M1r=0,M1c=0, M2r=0, M2c=0; /* matrices (rows and columns) */
+  size_t M1r=0,M1c=0, M2r=0, M2c=0; /* matrices (rows and columns) */
 
   /* opening files */
   f1 = openFile(argv[1],f1);
