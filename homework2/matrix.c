700054743cbf1bb1a2d0adea531460d80a7ceb9a
cjgc <chjugomez@utp.edu.co>
2017-09-12 16:12:57 -0500

    Pragma parallel region was optimized. Int data type was renamed by size_t.

--- homework2/matrix.c
+++ homework2/matrix.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define CHUNK 10 
 
 FILE * openFile(char const *fileName,FILE *f){
   /* This function will try to open a file */
@@ -23,7 +22,7 @@ float * buildMatrix(FILE *f,size_t &rows,size_t &columns){
 void getData(float *M,FILE *f){
   /* This function will capture data from plain text file to system memory */
   char *data = (char *)malloc(sizeof(char)), *newData = NULL,ch = ' ';
-  int dataSize = sizeof(char), Mindex = 0;
+  size_t dataSize = sizeof(char), Mindex = 0;
   while(!feof(f)){
     ch = fgetc(f);
     if(ch == ',' || ch == '\n'){
@@ -50,13 +49,11 @@ void hardrive(float *M,size_t Mr,size_t Mc){
      M -> Matrix, Mr -> Matrix rows, Mc -> Matrix columns
   */
   FILE *f = fopen("output.txt","w+");
-  for(size_t i=0;i<Mr;i++){
+  for(size_t i=0;i<Mr;i++)
     for(size_t j=0;j<Mc;j++){
-      if(j+1 == Mc) fprintf(f,"%.1f",M[i*Mc + j]);
+      if(j+1 == Mc) fprintf(f,"%.1f\n",M[i*Mc + j]);
       else fprintf(f,"%.1f,",M[i*Mc + j]);
     }
-    fprintf(f,"%c",'\n');
-  }
   fclose(f);
 }
 
@@ -67,25 +64,25 @@ void mulMatrices(float *M1,size_t M1r,size_t M1c,float *M2,size_t M2r,size_t M2c
      columns, M2r -> Matrix2 rows, M2c -> Matrix2 columns
   */
   if(M1c != M2r){printf("Matrices cannot be multiply!"); return;}
-  size_t M3size = M1r*M2c, M3index=0,i=0,j=0,k=0, chunk=CHUNK;
+  size_t M3size = M1r*M2c,i=0,j=0,k=0, chunk=M1r/8;
 	int tid = 0, noThreads = 0;
   float M3[M3size]; /* M3 -> Matrix3 will contain the result */
-  #pragma omp parallel private(i,j,k,M3index,tid,noThreads) shared(M3,chunk) num_threads(20)
-	{
-		tid = omp_get_thread_num();
-		if(tid == 0){
-			noThreads = omp_get_num_threads();
-			printf("Thread master! with id %d\n",tid);
-			printf("Invoked threads number %d",noThreads);
-		}
-		#pragma omp for schedule(dynamic,chunk)
-    for(i=0; i<M1r; i++)
-      for(j=0; j<M2c; j++,M3index++){
-        float data = 0.0;
-        for(k=0; k<M1c; k++) data = M1[i*M1c+k] * M2[k*M2c+j] + data;
-	M3[M3index] = data;
-      }
-	}
+  #pragma omp parallel private(i,j,k,tid,noThreads) shared(M3,chunk) num_threads(8)
+  {
+    tid = omp_get_thread_num();
+    if(tid == 0){
+      noThreads = omp_get_num_threads();
+      printf("Thread master! with id %d\n",tid);
+      printf("Invoked threads number %d",noThreads);
+    }
+    #pragma omp for schedule(dynamic,chunk)
+      for(i=0; i<M1r; i++)
+        for(j=0; j<M2c; j++){
+          float data = 0.0;
+          for(k=0; k<M1c; k++) data = M1[i*M1c+k] * M2[k*M2c+j] + data;
+          M3[i*M1c+j] = data;
+        }
+    }
   hardrive(M3,M1r,M2c);
 }
 
@@ -111,7 +108,7 @@ int main(int argc, char const *argv[]) {
   /* multiplying matrices */
   mulMatrices(M1,M1r,M1c,M2,M2r,M2c);
   clock_t end = clock();
-  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  float time_spent = (float)(end - begin) / CLOCKS_PER_SEC;
 
   /* freeing memory */
   free(M1);
