bdcd8955188e3fb86a9ac852a24a7d7e575d34b4
cjgc <chjugomez@utp.edu.co>
2017-09-22 15:49:34 -0500

    MatMul Kernel was added

--- openCV/DisplayImage.cpp
+++ openCV/DisplayImage.cpp
@@ -1,28 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <opencv2/opencv.hpp>
+#include <math.h>
+#include <cuda.h>
 
 using namespace cv;
 
-int main(int argc, char** argv )
-{
-    if ( argc != 2 )
-    {
+__global__ void matMul(double *image, double *resImage,const size_t& rows,const size_t& cols){
+	/* it will turn an image to grayscale */
+	int ti = blockIdx.y*blockDim.y+threadIdx.y;
+	int tj = blockIdx.x*blockDim.x+threadIdx.x;
+	if(ti < rows && tj < cols){
+		for(size_t k=0; k<rows; k++){
+			resImage[ti*rows + tj] = image[ti*rows + tj];
+		}	
+	}
+}
+
+int main(int argc, char** argv ){
+    if(argc != 2){
         printf("usage: DisplayImage.out <Image_Path>\n");
         return -1;
     }
 
     Mat image;
-    image = imread( argv[1], 1 );
+    image = imread(argv[1],1);
 
-    if ( !image.data )
-    {
+    if(!image.data){
         printf("No image data \n");
         return -1;
     }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
-
-    waitKey(0);
+    //namedWindow("Display Image", WINDOW_AUTOSIZE );
+    //imshow("Display Image", image);
+    //waitKey(0);
 
     return 0;
 }
