96effec9a3ee7b29bc6a76e3be1c0911cc3cb8a5
cjgc <chjugomez@utp.edu.co>
2017-09-22 20:07:49 -0500

    Code tweaks

--- homework3/cudaMatrix.cu
+++ homework3/cudaMatrix.cu
@@ -13,8 +13,7 @@ __global__ void matricesMul(double *m1, double *m2, double *m3)
     // Make sure we do not go out of bounds
     if(ti < rows && tj < cols){
 			double data= 0.0;
-			int k;
-      for(k=0;k<rows;k++) data += m1[ti*rows+k] * m2[k*cols+tj];
+      for(int k=0;k<rows;k++) data += m1[ti*rows+k] * m2[k*cols+tj];
       m3[ti*rows+tj] = data;
     }
 }
@@ -66,8 +65,8 @@ int main( int argc, char* argv[] ){
     // Number of thread blocks in matrix grid
 		size_t gridNum = ceil((double)n/1024);  // needed grid numbers to our problem
 		size_t gridR = ceil(sqrt(gridNum)); 		// grid rows	
-		size_t gridC = gridR										// gird cols
-    dim3 dimGrid(gridR,gridR,1);
+		size_t gridC = gridR										// grid cols
+    dim3 dimGrid(gridR,gridC,1);
 
     // Execute the kernel
     matricesMul<<<dimGrid,dimBlock>>>(d_m1, d_m2, d_m3);
