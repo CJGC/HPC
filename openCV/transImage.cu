94ecc695a382cfb12c18c74e70aa7492db429fde
cjgc <chjugomez@utp.edu.co>
2017-09-24 09:39:34 -0500

    Tabspace adjusted

--- openCV/transImage.cu
+++ openCV/transImage.cu
@@ -11,7 +11,7 @@
 using namespace cv;
 
 __global__ void matMul(double *image, double *resImage,const size_t& rows,const size_t& cols){
-/* it will multiply each pixel of given image per 2 */
+   /* it will multiply each pixel of given image per 2 */
    int ti = blockIdx.y*blockDim.y+threadIdx.y;
    int tj = blockIdx.x*blockDim.x+threadIdx.x;
    if(ti < rows && tj < cols){
@@ -24,55 +24,55 @@ __global__ void matMul(double *image, double *resImage,const size_t& rows,const 
 }
 
 int main(int argc, char** argv ){
-	if(argc != 2){
-		printf("usage: DisplayImage.out <Image_Path>\n");
-		return -1;
-	}
+   if(argc != 2){
+      printf("usage: DisplayImage.out <Image_Path>\n");
+      return -1;
+   }
 
-	Mat image;
-	image = imread(argv[1],1);
+   Mat image;
+   image = imread(argv[1],1);
 
-	if(!image.data){
-		printf("No image data \n");
-		return -1;
-	}
-	unsigned char *h_rawImage, *d_rawImage, *h_procImage, *d_procImage;
+   if(!image.data){
+      printf("No image data \n");
+      return -1;
+   }
+   unsigned char *h_rawImage, *d_rawImage, *h_procImage, *d_procImage;
 
-	/* Memory management */
-	Size imgSize = image.size();
-	size_t imgHeight, imgWidth;
-	imgHeight = imgSize.height;
-	imgWidth = imgSize.width;
-	size_t reqMem = imgHeight*imgWidth*image.channels()*sizeof(unsigned char);
-	h_rawImage = (unsigned char *)malloc(reqMem);
-	h_procImage = (unsigned char *)malloc(reqMem);
-	h_rawImage = image.data;	
-	cudaMalloc((void**)&d_rawImage,reqMem);
-	cudaMalloc((void**)&d_procImage,reqMem);	
-	dim3 blockSize(32,32,1);
-	size_t reqBlocks = ceil((double)reqMem/1024);
-	size_t blocksInX = ceil(sqrt(reqBlocks));
-	size_t blocksInY = blocksInX;
-	dim3 gridSize(blocksInX,blocksInY,1);
+   /* Memory management */
+   Size imgSize = image.size();
+   size_t imgHeight, imgWidth;
+   imgHeight = imgSize.height;
+   imgWidth = imgSize.width;
+   size_t reqMem = imgHeight*imgWidth*image.channels()*sizeof(unsigned char);
+   h_rawImage = (unsigned char *)malloc(reqMem);
+   h_procImage = (unsigned char *)malloc(reqMem);
+   h_rawImage = image.data;	
+   cudaMalloc((void**)&d_rawImage,reqMem);
+   cudaMalloc((void**)&d_procImage,reqMem);	
+   dim3 blockSize(32,32,1);
+   size_t reqBlocks = ceil((double)reqMem/1024);
+   size_t blocksInX = ceil(sqrt(reqBlocks));
+   size_t blocksInY = blocksInX;
+   dim3 gridSize(blocksInX,blocksInY,1);
 
-	/* Transfering data to device */
-	cudaMemcpy(d_rawImage,h_rawImage,reqMem,cudaMemcpyHostToDevice);
-	/* Operating */	
-	matMul<<<gridSize,blockSize>>>(d_rawImage,d_procImage,imgHeight,imgWidth);		
-	/* Recovering data to host */
-	cudaMemcpy(h_procImage,d_procImage,reqMem,cudaMemcpyDeviceToHost);
+   /* Transfering data to device */
+   cudaMemcpy(d_rawImage,h_rawImage,reqMem,cudaMemcpyHostToDevice);
+   /* Operating */	
+   matMul<<<gridSize,blockSize>>>(d_rawImage,d_procImage,imgHeight,imgWidth);		
+   /* Recovering data to host */
+   cudaMemcpy(h_procImage,d_procImage,reqMem,cudaMemcpyDeviceToHost);
 
-	/* Saving Image */
-	Mat procImage;
-	procImage.create(imgHeight,imgWidth,CV_8UC3);	
-	procImage.data = h_procImage;
-	imwrite("output.jpg",procImage);
+   /* Saving Image */
+   Mat procImage;
+   procImage.create(imgHeight,imgWidth,CV_8UC3);	
+   procImage.data = h_procImage;
+   imwrite("output.jpg",procImage);
 
-	/* Freeing memory */
-	cudaFree(d_rawImage);
-	cudaFree(d_procImage);
-	free(h_rawImage);
-	free(h_procImage);
-	return 0;
+   /* Freeing memory */
+   cudaFree(d_rawImage);
+   cudaFree(d_procImage);
+   free(h_rawImage);
+   free(h_procImage);
+   return 0;
 }
 
