0e18aa7d418fc643c50f648f6251a5b233fdd5e4
cjgc <chjugomez@utp.edu.co>
2017-09-27 19:39:01 -0500

    Handling error was introduced.

--- homework4/transImage.cu
+++ homework4/transImage.cu
@@ -3,22 +3,27 @@
 #include <opencv2/opencv.hpp>
 #include <math.h>
 #include <cuda.h>
-#define RED = 2
-#define GREEN = 1
-#define BLUE = 0
-#define chanDepth = 3
+#define RED 2
+#define GREEN 1
+#define BLUE 0
+#define chanDepth 3
 
 using namespace cv;
+
+__host__ void checkCudaState(cudaError_t& cudaState,const char *message){
+   /* it will print an error message if there is */
+   if(cudaState != cudaSuccess){ printf("%s",message); exit(-1);}
+}
 
 __global__ void matMul(unsigned char *image,unsigned char *resImage,int rows,int cols){
    /* it will multiply each pixel of given image per 2 */
    int ti = blockIdx.y*blockDim.y+threadIdx.y;
    int tj = blockIdx.x*blockDim.x+threadIdx.x;
    if(ti < rows && tj < cols){
-			int pos = (ti*rows + tj)*3;
-      resImage[pos+0] = image[pos+0]*2;
-      resImage[pos+1] = image[pos+1]*2;
-      resImage[pos+2] = image[pos+2]*2;
+      int pos = (ti*rows + tj)*chanDepth;
+      resImage[pos+BLUE] = 2;//image[pos+BLUE]*2;
+      resImage[pos+GREEN] = 2;//image[pos+GREEN]*2;
+      resImage[pos+RED] = 2;//image[pos+RED]*2;
    }
 }
 
@@ -30,37 +35,40 @@ int main(int argc, char** argv ){
 
    Mat image;
    image = imread(argv[1],1);
+   cudaError_t cudaState = cudaSuccess;
 
    if(!image.data){
       printf("No image data \n");
       return -1;
    }
-   unsigned char *h_rawImage, *d_rawImage, *h_procImage, *d_procImage;
+   unsigned char *h_rawImage, *d_rawImage,*h_procImage, *d_procImage;
 
    /* Memory management */
    Size imgSize = image.size();
-   int imgHeight, imgWidth;
-   imgHeight = imgSize.height;
-   imgWidth = imgSize.width;
+   int imgHeight = imgSize.height, imgWidth = imgSize.width;
    int reqMem = imgHeight*imgWidth*image.channels()*sizeof(unsigned char);
    h_rawImage = (unsigned char *)malloc(reqMem);
    h_procImage = (unsigned char *)malloc(reqMem);
-   h_rawImage = image.data;	
-   cudaMalloc((void**)&d_rawImage,reqMem);
-   cudaMalloc((void**)&d_procImage,reqMem);	
+   cudaState = cudaMalloc((void**)&d_rawImage,reqMem);
+   checkCudaState(cudaState,"Was not possible allocate memory for d_rawImage\n");
+   cudaState = cudaMalloc((void**)&d_procImage,reqMem);	
+   checkCudaState(cudaState,"Was not possible allocate memory for d_procImage\n");
+   h_rawImage = image.data; 
    dim3 blockSize(32,32,1);
    int reqBlocks = ceil((double)reqMem/1024);
    int blocksInX = ceil(sqrt(reqBlocks));
    int blocksInY = blocksInX;
    dim3 gridSize(blocksInX,blocksInY,1);
-
+ 
    /* Transfering data to device */
-   cudaMemcpy(d_rawImage,h_rawImage,reqMem,cudaMemcpyHostToDevice);
+   cudaState = cudaMemcpy(d_rawImage,h_rawImage,reqMem,cudaMemcpyHostToDevice);
+   checkCudaState(cudaState,"Was not possible copy data from h_rawImage to d_rawImage\n");
    /* Operating */	
-   matMul<<<gridSize,blockSize>>>(d_rawImage,d_procImage,imgHeight,imgWidth);		
+   matMul<<<gridSize,blockSize>>>(d_rawImage,d_procImage,imgHeight,imgWidth);
+   cudaDeviceSynchronize();
    /* Recovering data to host */
-   cudaMemcpy(h_procImage,d_procImage,reqMem,cudaMemcpyDeviceToHost);
-
+   cudaState = cudaMemcpy(h_procImage,d_procImage,reqMem,cudaMemcpyDeviceToHost);
+   checkCudaState(cudaState,"Was not possible copy data from d_procImage to h_procImage\n");
    /* Saving Image */
    Mat procImage;
    procImage.create(imgHeight,imgWidth,CV_8UC3);	
@@ -70,8 +78,8 @@ int main(int argc, char** argv ){
    /* Freeing memory */
    cudaFree(d_rawImage);
    cudaFree(d_procImage);
-   free(h_rawImage);
+   // h_rawImage is a pointer to Mat's buffer, when Mat's buffer is  destroyed 
+   // memory is freed
    free(h_procImage);
-   return 0;
 }
 
