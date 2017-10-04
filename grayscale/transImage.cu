67ec2cad19e480a3bf20529fef0a0c3d29291cb7
cjgc <chjugomez@utp.edu.co>
2017-10-04 15:38:39 -0500

    Grayscale source was carried to matricial form

--- grayscale/transImage.cu
+++ grayscale/transImage.cu
@@ -10,17 +10,22 @@
 
 using namespace cv;
 
-__host__ void checkCudaState(cudaError_t& cudaState,const char *message){
+__host__ void checkCudaState(cudaError_t& cudaState,const char *message,unsigned char *h_grayScale){
    /* it will print an error message if there is */
-   if(cudaState != cudaSuccess){ printf("%s",message); exit(-1);}
+   if(cudaState != cudaSuccess){
+      printf("%s",message);
+      free(h_grayScale);
+      exit(-1);
+   }
 }
 
 __global__ void grayScale(unsigned char *image,unsigned char *resImage,int rows,int cols){
    /* it will turn an image to gray scale image */
+   int ti = blockIdx.y*blockDim.y+threadIdx.y;
    int tj = blockIdx.x*blockDim.x+threadIdx.x;
-   if(tj < rows*cols){
-      int pos = (tj)*chanDepth;
-      resImage[tj] = image[pos+BLUE]*0.07 + image[pos+GREEN]*0.72 + image[pos+RED]*0.21;
+   if(ti < rows && tj < cols){
+      int pos = (ti*cols + tj)*chanDepth;
+      resImage[ti*cols + tj] = image[pos+BLUE]*0.07 + image[pos+GREEN]*0.72 + image[pos+RED]*0.21;
    }
 }
 
@@ -47,23 +52,24 @@ int main(int argc, char** argv ){
    int reqMemForGrayScaImg = imgHeight*imgWidth*sizeof(unsigned char);
    h_grayScale = (unsigned char *)malloc(reqMemForGrayScaImg);
    cudaState = cudaMalloc((void**)&d_rawImage,reqMemForRawImg);
-   checkCudaState(cudaState,"Was not possible allocate memory for d_rawImage\n");
+   checkCudaState(cudaState,"Was not possible allocate memory for d_rawImage\n",h_grayScale);
    cudaState = cudaMalloc((void**)&d_grayScale,reqMemForGrayScaImg);	
-   checkCudaState(cudaState,"Was not possible allocate memory for d_grayScale\n");
+   checkCudaState(cudaState,"Was not possible allocate memory for d_grayScale\n",h_grayScale);
    h_rawImage = image.data; 
-   dim3 blockSize(1024,1,1);
-   int reqBlocks = ceil((double)imgHeight*imgWidth/1024);
-   dim3 gridSize(reqBlocks,1,1);
+   dim3 blockSize(32,32,1);
+   int reqBlocksInX = ceil((double)imgHeight/32.0); 
+   int reqBlocksInY = ceil((double)imgWidth/32.0); 
+   dim3 gridSize(reqBlocksInX,reqBlocksInY,1);
  
    /* Transfering data to device */
    cudaState = cudaMemcpy(d_rawImage,h_rawImage,reqMemForRawImg,cudaMemcpyHostToDevice);
-   checkCudaState(cudaState,"Was not possible copy data from h_rawImage to d_rawImage\n");
+   checkCudaState(cudaState,"Was not possible copy data from h_rawImage to d_rawImage\n",h_grayScale);
    /* Operating */	
    grayScale<<<gridSize,blockSize>>>(d_rawImage,d_grayScale,imgHeight,imgWidth);
    cudaDeviceSynchronize();
    /* Recovering data to host */
    cudaState = cudaMemcpy(h_grayScale,d_grayScale,reqMemForGrayScaImg,cudaMemcpyDeviceToHost);
-   checkCudaState(cudaState,"Was not possible copy data from d_grayScale to h_grayScale\n");
+   checkCudaState(cudaState,"Was not possible copy data from d_grayScale to h_grayScale\n",h_grayScale);
    /* Saving Image */
    Mat procImage;
    procImage.create(imgHeight,imgWidth,CV_8UC1);	
