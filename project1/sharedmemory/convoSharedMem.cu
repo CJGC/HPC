4fd717973069b824bdb99d83cc2dc65d4542d297
cjgc <chjugomez@utp.edu.co>
2017-10-13 13:45:05 -0500

    Second mask_y is now considered in the sobel imaging process

--- project1/sharedmemory/convoSharedMem.cu
+++ project1/sharedmemory/convoSharedMem.cu
@@ -33,7 +33,7 @@ __device__ void setCoords(int w,uint by,uint bx,int& d,int& dY,int& dX,int& s,in
   s = sY * w + sX;
 }
 
-__global__ void sobeFilt(uchar *image,uchar *resImage,int width,int height,char* mask){
+__global__ void sobeFilt(uchar *image,uchar *resImage,int width,int height,char* mask_y,char* mask_x){
   //uint maskWidth = 3; //sqrt((double)sizeof(mask)/sizeof(char));
 	uint image_sWidth = blockWidth+maskWidth-1;
 	__shared__ uchar image_s[blockWidth+maskWidth-1][blockWidth+maskWidth-1];
@@ -53,16 +53,19 @@ __global__ void sobeFilt(uchar *image,uchar *resImage,int width,int height,char*
 	__syncthreads();
 
 
-	int Pvalue = 0;
+	int PvalueY = 0, PvalueX = 0;
 	uint row, col;
 	for(row = 0; row < maskWidth; row++)
-		for(col = 0; col < maskWidth; col++)
-			Pvalue += image_s[ty + row][tx + col] * mask[row * maskWidth + col];
+		for(col = 0; col < maskWidth; col++){
+      PvalueY += image_s[ty + row][tx + col] * mask_y[row * maskWidth + col];
+			PvalueX += image_s[ty + row][tx + col] * mask_x[row * maskWidth + col];
+    }
 
 	row = by*blockWidth + ty;
 	col = bx*blockWidth + tx;
 
 	if(row < height && col < width)
+    int Pvalue = sqrt((double)(PvalueY*PvalueY) + (double)(PvalueX*PvalueX));
 		resImage[row * width + col] = clamp(Pvalue);
 	__syncthreads();
 }
@@ -79,8 +82,8 @@ __global__ void grayScale(uchar *image,uchar *resImage,int rows,int cols){
 
 int main(int argc, char** argv ){
    if(argc != 3){
-	  printf("usage: %s <image> <numCases>\n",argv[0]);
-	  return -1;
+      printf("usage: %s <image> <numCases>\n",argv[0]);
+      return -1;
    }
 
    Mat image;
@@ -88,9 +91,10 @@ int main(int argc, char** argv ){
    cudaError_t cudaState = cudaSuccess;
 
    if(!image.data){
-	  printf("No image data \n");
-	  return -1;
+      printf("No image data \n");
+      return -1;
    }
+
    int cases = atoi(argv[2]);
    FILE *data= fopen("data.txt","w+");
    do{
@@ -103,9 +107,10 @@ int main(int argc, char** argv ){
      int reqMemForProcImg = imgHeight*imgWidth*sizeof(uchar);
      uchar *h_rawImage = NULL, *h_grayScale = NULL, *h_sobelImage = NULL;
      uchar *d_rawImage = NULL, *d_grayScale = NULL, *d_sobelImage = NULL;
-     char h_mask[] = {-1,0,1,-2,0,2,-1,0,1}, *d_mask = NULL;
-     uint maskSize = sizeof(h_mask);
-
+     char h_mask_y[] = {-1,-2,-1,0,0,0,1,2,1}, h_mask_x[] = {-1,0,1,-2,0,2,-1,0,1};
+     char *d_mask_y=NULL, *d_mask_x=NULL;
+     uint maskSize_y = sizeof(h_mask_y);
+     uint maskSize_x = sizeof(h_mask_x);
      h_grayScale = (uchar *)malloc(reqMemForProcImg);
      h_sobelImage = (uchar *)malloc(reqMemForProcImg);
 
@@ -115,54 +120,60 @@ int main(int argc, char** argv ){
      checkCudaState(cudaState,"Unallocated memory for d_grayScale\n");
      cudaState = cudaMalloc((void**)&d_sobelImage,reqMemForProcImg);
      checkCudaState(cudaState,"Unallocated memory for d_sobelImage\n");
-     cudaState = cudaMalloc((void**)&d_mask,maskSize);
-     checkCudaState(cudaState,"Unallocated memory for d_mask\n");
+     cudaState = cudaMalloc((void**)&d_mask_y,maskSize_y);
+     checkCudaState(cudaState,"Unallocated memory for d_mask_y\n");
+     cudaState = cudaMalloc((void**)&d_mask_x,maskSize_x);
+     checkCudaState(cudaState,"Unallocated memory for d_mask_x\n");
 
-     if(d_rawImage != NULL && d_grayScale != NULL && d_sobelImage != NULL && d_mask != NULL){
-  	 /* Setting kernel properties */
-  	 h_rawImage = image.data;
-  	 dim3 blockSize(32,32,1);
-  	 int reqBlocksInX = ceil((double)imgHeight/32.0);
-  	 int reqBlocksInY = ceil((double)imgWidth/32.0);
-  	 dim3 gridSize(reqBlocksInY,reqBlocksInX,1);
+     if(d_rawImage != NULL && d_grayScale != NULL && d_sobelImage != NULL && d_mask_x != NULL && d_mask_y != NULL){
+       /* Setting kernel properties */
+       h_rawImage = image.data;
+       dim3 blockSize(32,32,1);
+       int reqBlocksInX = ceil((double)imgHeight/32.0);
+       int reqBlocksInY = ceil((double)imgWidth/32.0);
+       dim3 gridSize(reqBlocksInY,reqBlocksInX,1);
 
-     start = clock();
-  	 /* Transfering and processing data to obtain grayimage */
-  	 cudaState = cudaMemcpy(d_rawImage,h_rawImage,reqMemForRawImg,cudaMemcpyHostToDevice);
-  	 checkCudaState(cudaState,"Impossible copy data from h_rawImage to d_rawImage\n");
-  	 grayScale<<<gridSize,blockSize>>>(d_rawImage,d_grayScale,imgHeight,imgWidth);
-  	 cudaDeviceSynchronize();
-  	 /* Transfering and processing data to obtain sobel image */
-  	 cudaState = cudaMemcpy(d_mask,h_mask,maskSize,cudaMemcpyHostToDevice);
-  	 checkCudaState(cudaState,"Impossible copy data from h_mask to d_mask\n");
-  	 sobeFilt<<<gridSize,blockSize>>>(d_grayScale,d_sobelImage,imgWidth,imgHeight,d_mask);
-  	 cudaDeviceSynchronize();
+       start = clock();
+       /* Transfering and processing data to obtain grayimage */
+       cudaState = cudaMemcpy(d_rawImage,h_rawImage,reqMemForRawImg,cudaMemcpyHostToDevice);
+       checkCudaState(cudaState,"Impossible copy data from h_rawImage to d_rawImage\n");
+       grayScale<<<gridSize,blockSize>>>(d_rawImage,d_grayScale,imgHeight,imgWidth);
+       cudaDeviceSynchronize();
+       /* Transfering and processing data to obtain sobel image */
+       cudaState = cudaMemcpy(d_mask_y,h_mask_y,maskSize_y,cudaMemcpyHostToDevice);
+       checkCudaState(cudaState,"Impossible copy data from h_mask_y to d_mask_y\n");
+       cudaState = cudaMemcpy(d_mask_x,h_mask_x,maskSize_x,cudaMemcpyHostToDevice);
+       checkCudaState(cudaState,"Impossible copy data from h_mask_x to d_mask_x\n");
+       sobeFilt<<<gridSize,blockSize>>>(d_grayScale,d_sobelImage,imgWidth,imgHeight,d_mask_y,d_mask_x);
+       cudaDeviceSynchronize();
 
-  	 /* Recovering data of grayScale image to h_grayScale */
-  	 cudaState = cudaMemcpy(h_grayScale,d_grayScale,reqMemForProcImg,cudaMemcpyDeviceToHost);
-  	 checkCudaState(cudaState,"Impossible copy data from d_grayScale to h_grayScale\n");
-  	 /* Recovering data of sobelImage to h_sobelImage */
-  	 cudaState = cudaMemcpy(h_sobelImage,d_sobelImage,reqMemForProcImg,cudaMemcpyDeviceToHost);
-  	 checkCudaState(cudaState,"Impossible copy data from d_sobelImage to h_sobelImage\n");
-     end = clock();
-     usedTime = ((double)(end - start))/ CLOCKS_PER_SEC;
-     fprintf(data,"%f\n",usedTime);
+       /* Recovering data of grayScale image to h_grayScale */
+       cudaState = cudaMemcpy(h_grayScale,d_grayScale,reqMemForProcImg,cudaMemcpyDeviceToHost);
+       checkCudaState(cudaState,"Impossible copy data from d_grayScale to h_grayScale\n");
+       /* Recovering data of sobelImage to h_sobelImage */
+       cudaState = cudaMemcpy(h_sobelImage,d_sobelImage,reqMemForProcImg,cudaMemcpyDeviceToHost);
+       checkCudaState(cudaState,"Impossible copy data from d_sobelImage to h_sobelImage\n");
+       end = clock();
+       usedTime = ((double)(end - start))/ CLOCKS_PER_SEC;
+       fprintf(data,"%f\n",usedTime);
 
-  	 /* Saving Image */
-  	 Mat grayscaleImage, sobelImage;
-  	 grayscaleImage.create(imgHeight,imgWidth,CV_8UC1);
-  	 sobelImage.create(imgHeight,imgWidth,CV_8UC1);
-  	 grayscaleImage.data = h_grayScale;
-  	 sobelImage.data = h_sobelImage;
-  	 imwrite("grayscale.jpg",grayscaleImage);
-  	 imwrite("sobel.jpg",sobelImage);
+       /* Saving Images */
+       Mat grayscaleImage, sobelImage;
+       grayscaleImage.create(imgHeight,imgWidth,CV_8UC1);
+       sobelImage.create(imgHeight,imgWidth,CV_8UC1);
+       grayscaleImage.data = h_grayScale;
+       sobelImage.data = h_sobelImage;
+       imwrite("grayscale.jpg",grayscaleImage);
+       imwrite("sobel.jpg",sobelImage);
      }
 
      /* Freeing device's memory */
      if(d_rawImage != NULL) cudaFree(d_rawImage);
      if(d_grayScale != NULL) cudaFree(d_grayScale);
      if(d_sobelImage != NULL) cudaFree(d_sobelImage);
-     if(d_mask != NULL) cudaFree(d_mask);
+     if(d_mask_x != NULL) cudaFree(d_mask_x);
+     if(d_mask_y != NULL) cudaFree(d_mask_y);
+
      /* Freeing host's memory */
      // h_rawImage is a pointer to Mat's buffer, when Mat's buffer is  destroyed
      // memory is freed
