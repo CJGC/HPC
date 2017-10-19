6e971ac6788443dc9eed92fd992613100c20e920
cjgc <chjugomez@utp.edu.co>
2017-10-18 20:30:25 -0500

    Unnecessary threads processing is now solved

--- project1/sharedmemory/convoSharedMem.cu
+++ project1/sharedmemory/convoSharedMem.cu
@@ -56,24 +56,21 @@ __global__ void sobeFilt(uchar *image,uchar *resImage,int width,int height,char*
     else image_s[destY][destX] = 0;
   }
   __syncthreads();
-
-
-  int PvalueY = 0, PvalueX = 0;
-  uint row, col;
-  for(row = 0; row < maskWidth; row++)
-    for(col = 0; col < maskWidth; col++){
-      PvalueY += image_s[ty + row][tx + col] * mask_y[row * maskWidth + col];
-      PvalueX += image_s[ty + row][tx + col] * mask_x[row * maskWidth + col];
-    }
-
-  row = by*blockWidth + ty;
-  col = bx*blockWidth + tx;
+  
+  uint row = by*blockWidth + ty;
+  uint col = bx*blockWidth + tx;
 
   if(row < height && col < width){
+    int PvalueY = 0, PvalueX = 0;
+    for(uint i = 0; i < maskWidth; i++)
+      for(uint j = 0; j < maskWidth; j++){
+        PvalueY += image_s[ty + i][tx + j] * mask_y[i * maskWidth + j];
+        PvalueX += image_s[ty + i][tx + j] * mask_x[i * maskWidth + j];
+      }
+
     int Pvalue = sqrt((double)(PvalueY*PvalueY) + (double)(PvalueX*PvalueX));
-    resImage[row * width + col] = clamp(Pvalue);
+    resImage[row* width + col] = clamp(Pvalue);
   }
-  __syncthreads();
 }
 
 __global__ void grayScale(uchar *image,uchar *resImage,int rows,int cols){
